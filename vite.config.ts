8f3d589f2351ac8c0a56ea4be06a53af17c8b84c
seyedalimosavi <86632701+seyedalimosavi@users.noreply.github.com>
2026-08-09 07:38:20 +0330

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
+  base: "/shift-shafaf-web/",
   tanstackStart: {
     // Redirect TanStack Start's bundled server entry to src/server.ts (our SSR error wrapper).
     // nitro/vite builds from this
